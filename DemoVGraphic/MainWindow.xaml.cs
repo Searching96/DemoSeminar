7dd1a31b67ee864622069ccccfe28299b1ce5e8e
Searching96 <nguyenphucthinh2k5@gmail.com>
2024-10-27 15:43:31 +0700

    Added movable canvas

--- DemoVGraphic/MainWindow.xaml.cs
+++ DemoVGraphic/MainWindow.xaml.cs
@@ -21,22 +21,39 @@ namespace DemoVGraphic
             InitializeComponent();
         }
 
+        private Point _previousMousePosition;
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _previousMousePosition = e.GetPosition(this);
+            MainCanvas.CaptureMouse();
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (MainCanvas.IsMouseCaptured)
+            {
+                Point currentPosition = e.GetPosition(this);
+                double offsetX = currentPosition.X - _previousMousePosition.X;
+                double offsetY = currentPosition.Y - _previousMousePosition.Y;
+
+                CanvasTranslateTransform.X += offsetX;
+                CanvasTranslateTransform.Y += offsetY;
+
+                _previousMousePosition = currentPosition;
+            }
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MainCanvas.ReleaseMouseCapture();
+        }
+
         private void Canvas_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            // Lấy ScaleTransform hiện tại hoặc khởi tạo mới
-            var scaleTransform = MainCanvas.RenderTransform as ScaleTransform;
-            if (scaleTransform == null)
-            {
-                scaleTransform = new ScaleTransform();
-                MainCanvas.RenderTransform = scaleTransform;
-            }
-
-            // Thiết lập hệ số zoom
-            double zoomFactor = e.Delta > 0 ? 1.1 : 1 / 1.1;
-
-            // Cập nhật tỷ lệ scale
-            scaleTransform.ScaleX *= zoomFactor;
-            scaleTransform.ScaleY *= zoomFactor;
+            double zoomFactor = e.Delta > 0 ? 1.1 : 0.9;
+            CanvasScaleTransform.ScaleX *= zoomFactor;
+            CanvasScaleTransform.ScaleY *= zoomFactor;
         }
     }
 }
